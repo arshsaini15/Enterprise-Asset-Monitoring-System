dcc7500ca5a3520d343b1e0fd5e3cfc1293504aa
arshsaini15 <arshsaini015@gmail.com>
2026-04-30 18:28:35 +0530

    Register User DTO

---
+++ src/main/java/com/eams/user/dto/RegisterRequestDTO.java
@@ -0,0 +1,29 @@
+package com.eams.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDTO {
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+}
