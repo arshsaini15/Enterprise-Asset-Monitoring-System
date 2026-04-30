68fd4e97ea3602ce42c6843fd61d7db5b92e2e3e
arshsaini15 <arshsaini015@gmail.com>
2026-04-30 18:23:00 +0530

    User Repository

---
+++ src/main/java/com/eams/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.eams.user.repository;
+
+import com.eams.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByEmail(String email);
+}
