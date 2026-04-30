aff44417245642cb8ebd528d44c5060642dbdaab
arshsaini15 <arshsaini015@gmail.com>
2026-04-30 18:30:37 +0530

    Login Request DTO

---
+++ src/main/java/com/eams/user/dto/LoginRequestDTO.java
@@ -0,0 +1,22 @@
+package com.eams.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Pass email")
+    private String email;
+
+    @NotBlank(message = "Pass password")
+    private String password;
+
+}
