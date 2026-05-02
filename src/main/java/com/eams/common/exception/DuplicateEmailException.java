861adf2d82662d5d7faa7840ab2ed7581c6a9cb7
arshsaini15 <arshsaini015@gmail.com>
2026-05-02 13:36:03 +0530

    Updated UserController + UserService, thrown custom Exceptions

---
+++ src/main/java/com/eams/common/exception/DuplicateEmailException.java
@@ -0,0 +1,9 @@
+package com.eams.common.exception;
+
+public class DuplicateEmailException extends RuntimeException {
+
+    public DuplicateEmailException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/eams/user/repository/UserRepository.java
+++ src/main/java/com/eams/user/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.eams.user.repository;
 
 import com.eams.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByEmail(String email);
-    public boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
