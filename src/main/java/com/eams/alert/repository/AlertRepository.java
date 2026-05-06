25e3dbd3da9598a8d01860e62d2928698b940a5f
arshsaini15 <arshsaini015@gmail.com>
2026-05-06 10:59:44 +0530

    Alert Repository Updated

--- src/main/java/com/eams/alert/repository/AlertRepository.java
+++ src/main/java/com/eams/alert/repository/AlertRepository.java
@@ -1,6 +1,17 @@
 package com.eams.alert.repository;
 
+import com.eams.alert.enums.AlertStatus;
+import com.eams.alert.enums.AlertType;
 import com.eams.alert.model.Alert;
+import com.eams.asset.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlertRepository extends JpaRepository<Alert, Long> {}
+import java.util.Optional;
+
+public interface AlertRepository extends JpaRepository<Alert, Long> {
+    Optional<Alert> findByAssetAndTypeAndStatus(
+            Asset asset,
+            AlertType type,
+            AlertStatus status
+    );
+}
